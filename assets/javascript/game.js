3de3ed990ffe0d7b5b736b6bd7d6ebbc7f5f7a1b
J.McKinney <jsmckinney89@gmail.com>
2018-12-19 19:04:31 -0500

    updated via comments

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,46 +1,54 @@
-var option = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-
 var winCount = document.getElementById("win");
 var lossCount = document.getElementById("loss");
 var guessCounter = document.getElementById("guessLeft");
 var letterDisplay = document.getElementById("letter");
 
+var goalStr = 'abcdefghijklmnopqrstuvwxyz'
 var win = 0;
 var loss = 0;
 var guessLeft = 10;
 var letter = [];
 
+
 function createObjective() {
-    objective = option[Math.floor(Math.random() * option.length)];
+    objective = goalStr[Math.floor(Math.random() * goalStr.length)];
+    console.log(objective);
 }
+
+function restart() {
+    guessLeft = 10;
+    letter = [];
+    createObjective();
+}
+
 createObjective();
 
+
 document.onkeyup = function (event) {
-/*keystroke will cipher through option array to verify key is lowercase letter.  
- if it is a lowercase letter it will run the code;
- if letter has already been guessed it will not register another guess */
+    if (event.keyCode >= 65 && event.keyCode <= 90) {
+        var guess = event.key;
+        if (guess === objective) {
+            win++;
+            winCount.textContent = win;
+            restart();
+        }
+        else {
+            if (letter.indexOf(guess) === -1) {
+                letter.push(guess);
+                letterDisplay.textContent = letter;
+                guessLeft--;
+                guessCounter.textContent = guessLeft;
+                if (guessLeft === 0) {
+                    loss++;
+                    lossCount.textContent = loss;
+                    restart();
+                }
+            }
 
-    letter.push(event.key);
-    letterDisplay.textContent = letter;
-    var guess = event.key;
-
-    if (guess === objective) {
-        win++;
-        winCount.textContent = win;
-        guessLeft = 10;
-        letter = [];
-        createObjective();
-    }
-    else {
-        guessLeft--;
-        guessCounter.textContent = guessLeft;
-        if (guessLeft === 0) {
-            loss++;
-            lossCount.textContent = loss;
-            guessLeft = 10;
-            letter = [];
-            createObjective();
 
         }
     }
+
+
 }
+
